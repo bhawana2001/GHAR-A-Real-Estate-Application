1ca25499d44bf038e40c9b963bea821aba80515d
bhawana2001 <gaur.bhawana2019@gmail.com>
2023-11-26 14:40:57 +0530

    Initial Setup of Backend

---
+++ ghar/src/main/java/com/ghar/ghar/GharApplication.java
@@ -0,0 +1,14 @@
+package com.ghar.ghar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GharApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(GharApplication.class, args);
+	}
+
+}
---
+++ ghar/src/test/java/com/ghar/ghar/GharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghar.ghar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
